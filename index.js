8df413ace00e907550793041734f652410f17e2c
Ygor Costa <ygorcosta@MacBook-Air-do-Ygor.local>
2024-01-04 13:45:17 -0300

    primeiros esboços

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Ygor")
+let nome = prompt("Por favor, digite o nome do jogador: ")
+let vitorias = prompt("Agora digite o número de vitórias: ")
+let derrotas = prompt("Qual o número de derrotas: ")
+
+function resultado (vitorias, derrotas) {
+    return vitorias - derrotas
+}
